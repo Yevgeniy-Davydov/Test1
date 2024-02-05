fb57a17fce8ed9869918130f138f214488b4f3bb
Yevgeniy-Davydov <131383721+Yevgeniy-Davydov@users.noreply.github.com>
2024-02-05 22:59:39 +0100

    index modified, script is linked now

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 "use strict"
 
 
-console.log("Hi!");
-console.log("Hi again");
+// console.log("Hi!");
+// console.log("Hi again");
 
-let c = 23;
-let d = 2;
-console.log(c+d);
-console.log(c*d)
+// let c = 23;
+// let d = 2;
+// console.log(c+d);
+// console.log(c*d);
+
+// приклад об'єкту
+const priceData = {
+    Apples: '23.4',
+    BANANAS: '48',
+    oRAngGEs: '48.7584',
+  };
+  
+  let updatedPriceData = {};
+
+  console.log(priceData)
+  
+  // ваш код
+  
+//   console.log(updatedPriceData) // {apples: '23.40', bananas: '48.00', oranges: '48.76'}
